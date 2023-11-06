b9002b2b1a7dd4cb407f0fcb1571455cf4501655
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-06 22:50:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     is_self_employed=st.selectbox('Self_employed', [True,False,None])
     is_urban=st.selectbox('Urban', [True,False,None])
     credit_history=st.selectbox('Credit_History',[True,False,None])
-    df_selected=get_data.select_data(size=x,is_graduate,is_married,is_female,is_self_employed,is_urban,credit_history)
+    df_selected=get_data.select_data(x,is_graduate,is_married,is_female,is_self_employed,is_urban,credit_history)
     st.dataframe(df_selected)
     return None
     # radio('Pick one:', ['nose ','ear'])
