94aed5c43d797a40f873dab7cd3afff7bd059e31
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-06 21:18:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def main():
+    st.button('Hit me')
+
+    st.checkbox('Check me out')
+    st.radio('Pick one:', ['nose','ear'])
+    st.selectbox('Select', [1,2,3])
+    st.multiselect('Multiselect', [1,2,3])
+    st.slider('Slide me', min_value=0, max_value=10)
+    st.select_slider('Slide to select', options=[1,'2'])
+    st.text_input('Enter some text')
+    st.number_input('Enter a number')
+    st.text_area('Area for textual entry')
+    st.date_input('Date input')
+    st.time_input('Time entry')
+    st.file_uploader('File uploader')
+    
+    st.camera_input("一二三,茄子!")
+    st.color_picker('Pick a color')
+
+
+
+main()
 
 
 
