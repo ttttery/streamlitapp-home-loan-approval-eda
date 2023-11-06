ade363e91ca3bc970bf19789e2a810aba4faa81b
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-06 22:59:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,21 +17,10 @@ def main():
     credit_history=st.sidebar.selectbox('Credit_History',[None,True,False])
     df_selected=get_data.select_data(x,is_graduate,is_married,is_female,is_self_employed,is_urban,credit_history)
     st.dataframe(df_selected)
+    fig,ax=plt.subplot()
+    ax.plot(df_selected)
+    st.pyplot(fig)
     return None
-    # radio('Pick one:', ['nose ','ear'])
-    # st.selectbox('Select', [ 1, 2,3])
-    # st.multiselect('Multiselect', [ 1, 2,3])
-    # st.slider('Slide me', min_value=0, max_value=10)
-    # st.select_slider('Slide to select', options=[ 1,'2'])
-    # st.text_input('Enter some text')
-    # st.number_input('Enter a number')
-    # st.text_area('Area for textual entry')
-    # st.date_input('Date input')
-    # st.time_input('Time entry')
-    # st.file_uploader('File uploader')
-    #
-    # st.camera_input("一二三,茄子!")
-    # st.color_picker('Pick a color')
 
 main()
 
