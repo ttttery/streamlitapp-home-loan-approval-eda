8505aa4a2f5c48a62eff316f608d8fddae8ba121
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-06 23:34:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,35 @@ import streamlit as st
 import wash_data
 import get_data
 
-def main():
-
-    x=st.sidebar.slider('The size of data:',0.0,1.0,1.0,0.01)
-    df=wash_data.wash_data()
-    is_graduate=st.sidebar.selectbox('Graduate', [None,True,False])
-    is_married=st.sidebar.selectbox('Married', [None,True,False])
-    is_female=st.sidebar.selectbox('Female', [None,True,False])
-    is_self_employed=st.sidebar.selectbox('Self_employed', [None,True,False])
-    is_urban=st.sidebar.selectbox('Urban', [None,True,False])
-    credit_history=st.sidebar.selectbox('Credit_History',[None,True,False])
-    df_selected=get_data.select_data(x,is_graduate,is_married,is_female,is_self_employed,is_urban,credit_history)
+def page_home():
+    x = st.sidebar.slider('The size of data:', 0.0, 1.0, 1.0, 0.01)
+    df = wash_data.wash_data()
+    is_graduate = st.sidebar.selectbox('Graduate', [None, True, False])
+    is_married = st.sidebar.selectbox('Married', [None, True, False])
+    is_female = st.sidebar.selectbox('Female', [None, True, False])
+    is_self_employed = st.sidebar.selectbox('Self_employed', [None, True, False])
+    is_urban = st.sidebar.selectbox('Urban', [None, True, False])
+    credit_history = st.sidebar.selectbox('Credit_History', [None, True, False])
+    df_selected = get_data.select_data(x, is_graduate, is_married, is_female, is_self_employed, is_urban,
+                                       credit_history)
     st.dataframe(df_selected)
-    fig,ax=plt.subplot()
-    ax.plot(df_selected)
-    st.pyplot(fig)
+
+
     return None
 
+def page_plot_pie():
+    st.title('Pie Graph')
+
+def main():
+
+    session_state=st.session_state
+    if 'page' not in session_state:
+        session_state['page']='Home'
+    page=st.sidebar.radio('Navigate',['Home','Plot_pie'])
+
+    if page=='Home':
+        page_home()
+    elif page=='About':
+        page_plot_pie()
+
 main()
-
-
-
-
-
-
