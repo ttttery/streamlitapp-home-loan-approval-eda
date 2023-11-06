5af6609d557357ca7c1eb240bc7a04a76be8b914
dionysus23334 <134669199+dionysus23334@users.noreply.github.com>
2023-11-06 22:55:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ def main():
 
     x=st.sidebar.slider('The size of data:',0.0,1.0,1.0,0.01)
     df=wash_data.wash_data()
-    is_graduate=st.sidebar.selectbox('Graduate', [True,False,None])
-    is_married=st.sidebar.selectbox('Married', [True,False,None])
-    is_female=st.sidebar.selectbox('Female', [True,False,None])
-    is_self_employed=st.sidebar.selectbox('Self_employed', [True,False,None])
-    is_urban=st.sidebar.selectbox('Urban', [True,False,None])
-    credit_history=st.sidebar.selectbox('Credit_History',[True,False,None])
+    is_graduate=st.sidebar.selectbox('Graduate', [None,True,False])
+    is_married=st.sidebar.selectbox('Married', [None,True,False])
+    is_female=st.sidebar.selectbox('Female', [None,True,False])
+    is_self_employed=st.sidebar.selectbox('Self_employed', [None,True,False])
+    is_urban=st.sidebar.selectbox('Urban', [None,True,False])
+    credit_history=st.sidebar.selectbox('Credit_History',[None,True,False])
     df_selected=get_data.select_data(x,is_graduate,is_married,is_female,is_self_employed,is_urban,credit_history)
     st.dataframe(df_selected)
     return None
